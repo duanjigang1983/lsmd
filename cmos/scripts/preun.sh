04a5aa4ffd543302dce663a5a5f2c1b143b9e141
duanjigangcme <duanjigang1983@gmail.com>
2013-09-17 07:18:15 +0800

    updating cmos

--- cmos/scripts/preun.sh
+++ cmos/scripts/preun.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-name=`hostname`
-tt=`date "+%Y-%m-%d %H:%M:%S"`
-title="cmos on $name was uninstalled at $tt"
 exec > /tmp/cmos_uninstall.log 2>&1
-
 if [ "$1" = "0" ]; then
-        rm /service/tops-cmos || exit 1
-        /usr/local/bin/svc -dx /etc/service/tops-cmos /etc/service/tops-cmos/log || exit 1
+        rm /service/cmos || exit 1
+        /usr/local/bin/svc -dx /etc/service/cmos /etc/service/cmos/log || exit 1
 fi
 
-#if [ -f "/bin/mailx" ]
-#then
-#	echo $title | /bin/mailx -s "[pkg]$title" newscoo@alibaba-inc.com  -c jigang.djg@taobao.com  || exit 0
-#fi
