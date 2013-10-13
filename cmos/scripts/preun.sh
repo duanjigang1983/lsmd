e5ba91c3f8051df7aebbcf328dec5d9171b63acd
duanjigang <duanjigang1983@gmail.com>
2013-10-13 13:00:40 +0800

    updating

--- cmos/scripts/preun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-exec > /tmp/cmos_uninstall.log 2>&1
-if [ "$1" = "0" ]; then
-        rm /service/cmos || exit 1
-        /usr/local/bin/svc -dx /etc/service/cmos /etc/service/cmos/log || exit 1
-fi
-
